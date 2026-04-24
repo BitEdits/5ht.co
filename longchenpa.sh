d47a852396808f4458731726ebe2cc7f023db8e0
5HT <maxim@synrc.com>
2026-04-24 10:58:21 +0000

    new erpuno.sh downloader

--- longchenpa.sh
+++ longchenpa.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
-git clone git@github.com:longchenpa/bdrc
+git clone git@github.com:longchenpa/buda
 git clone git@github.com:longchenpa/longchenpa.guru
 git clone git@github.com:longchenpa/tonpa.guru
+git clone git@github.com:longchenpa/osal
 git clone git@github.com:longchenpa/wylie
-git clone git@github.com:5HT/5HT.co
+git clone git@github.com:longchenpa/confessions
+
