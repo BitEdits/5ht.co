edc5c85b6c213dc879375e2eb7eaaef047300136
5HT <maxim@synrc.com>
2026-04-24 11:09:10 +0000

    new axiosis.sh downloader

--- groupoid.sh
+++ groupoid.sh
@@ -1,14 +1,28 @@
 #/bin/sh
 
 git clone git@github.com:groupoid/alonzo
-git clone git@github.com:groupoid/anders
-git clone git@github.com:groupoid/cafe
-git clone git@github.com:groupoid/CCHM
-git clone git@github.com:groupoid/cubical.systems
-git clone git@github.com:groupoid/formal.uno
-git clone git@github.com:groupoid/groupoid.space
+git clone git@github.com:groupoid/yves
+
 git clone git@github.com:groupoid/henk
-git clone git@github.com:groupoid/hopf
-git clone git@github.com:groupoid/languages
+git clone git@github.com:groupoid/frank
+git clone git@github.com:groupoid/christine
+git clone git@github.com:groupoid/per
+git clone git@github.com:groupoid/laurent
+git clone git@github.com:groupoid/anders
+git clone git@github.com:groupoid/dan
+git clone git@github.com:groupoid/urs
+git clone git@github.com:groupoid/fabien
+git clone git@github.com:groupoid/jack
+
+git clone git@github.com:groupoid/tim
+git clone git@github.com:groupoid/max
+git clone git@github.com:groupoid/eijiro
+git clone git@github.com:groupoid/manfred
+
+git clone git@github.com:groupoid/robin
+git clone git@github.com:groupoid/leslie
+git clone git@github.com:groupoid/andrea
+
+
 
 
